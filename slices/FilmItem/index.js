89cc4159b9346a8c0a45dd3e2cb8f421f3f7cb92
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-09-13 01:01:19 +0200

    small adjustment

--- slices/FilmItem/index.js
+++ slices/FilmItem/index.js
@@ -3,13 +3,14 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react'
 import { PrismicNextImage } from "@prismicio/next";
 
 const FilmItem = ({ slice }) => {
+  console.log(slice)
   return(
     <div className='film-items'>
       <h1>{slice.primary.title}</h1>
       <div className='wrapper'>
         {slice.items.map((item, i) => {
           return(
-            <PrismicLink className='film-item' href={`/films/${item.link?.slug}`}>
+            <PrismicLink className='film-item' href={`/films/${item.link?.uid}`}>
               <div className='image'>
                 <PrismicNextImage field={item.image} layout="fill" />
               </div>
