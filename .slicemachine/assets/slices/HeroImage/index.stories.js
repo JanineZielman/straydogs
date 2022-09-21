f685397e3b5137273c32f488d5f2fcc407b2c750
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-09-21 13:06:27 +0200

    hero alignment

--- .slicemachine/assets/slices/HeroImage/index.stories.js
+++ .slicemachine/assets/slices/HeroImage/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587614295999-6c1c13675117"},"size":"small"},"slice_type":"hero_image","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1494173853739-c21f58b16055"},"size":"big","vertical_position":"top"},"slice_type":"hero_image","id":"_Default"}} />
 _Default.storyName = ''
--- slices/HeroImage/index.js
+++ slices/HeroImage/index.js
@@ -5,7 +5,7 @@ import { PrismicNextImage } from "@prismicio/next";
 const HeroImage = ({ slice }) => {
   return(
     <div className={`hero-image ${slice.primary.size}`}>
-      <div className='img-wrapper'>
+      <div className={`img-wrapper ${slice.primary.vertical_position}`}>
         <PrismicNextImage field={slice.primary.image} layout="fill" />
       </div>
     </div>
